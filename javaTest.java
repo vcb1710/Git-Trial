69b28884ce081526ea00000d48ce469ff06da136
varun <vcb1710@gmail.com>
2020-08-26 06:51:57 +0530

    javaTest file changed and commited

--- javaTest.java
+++ javaTest.java
@@ -3,7 +3,7 @@ public class javaTest {
 	public static void main(String [] args) {
 
 	for(int i = 0 ; i < 11 ; i++){
-	System.out.println("Namaste India");
+	System.out.println("Namaste India ///");
 	}
      
 
